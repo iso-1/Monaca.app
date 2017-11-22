47e8fa8c6bd0ab75561119f837dce722f1c1168d
iso-1 <kd1242983@st.kobedenshi.ac.jp>
2017-11-22 12:51:46 +0900

    手動スヌーズ状態

--- www/js/app.js
+++ www/js/app.js
@@ -16,10 +16,15 @@ function addTodo(camera_url) {
         img_tag = "<img src='" + camera_url + "'>";
     }
     $.mobile.changePage($("#list-page"));
-    $("#todo-list").append("<li>" + img_tag + "<h3>" + title + "</h3><p>" + body + "</p></li>")
+    $("#todo-list").append("<li>" + img_tag + 
+    "<h3>" + title + "</h3><p>" + body + "</p></li>")
     $("#todo-list").listview('refresh');
-    $('#timer').yycountdown({endDateTime:'2020/07/24 00:00:00'});
+    $('#timer').yycountdown({endDateTime:'2010/07/24 00:00:00'});
 };
+
+
+
+
 
 
 var strDate = $("date-deadline").text();  // HTMLフォームの文字列
