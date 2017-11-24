599e4a55b5d10d8eb0aa7a28f083c085a46c4170
iso-1 <kd1242983@st.kobedenshi.ac.jp>
2017-11-24 23:40:07 +0900

    ダイアログ処理を連続で可能 ToDoリストを保存した後に秒数を入力しようとするとバグが発生中

--- www/js/app.js
+++ www/js/app.js
@@ -1,25 +1,14 @@
-function addTodoPicture() {
-    navigator.camera.getPicture(addTodo, function() {
-        alert("Failed to get camera.");
-    }, {
-        quality : 50,
-        destinationType : Camera.DestinationType.FILE_URI,
-        targetWidth : 100,
-        targetHeight : 100
-    });
-}
 function addTodo(camera_url) {
-    var title = $("#todo-title").val();
-    var body = $("#todo-body").val();
-    var img_tag = "";
-    if (camera_url) {
-        img_tag = "<img src='" + camera_url + "'>";
-    }
-    $.mobile.changePage($("#list-page"));
-    $("#todo-list").append("<li>" + img_tag + 
-    "<h3>" + title + "</h3><p>" + body + "</p></li>")
-    $("#todo-list").listview('refresh');
-    $('#timer').yycountdown({endDateTime:'2010/07/24 00:00:00'});
+     var title = $("#todo-title").val();
+     var body = $("#todo-body").val();
+     var img_tag = "";
+     if (camera_url) {
+         img_tag = "<img src='" + camera_url + "'>";
+     }
+     $.mobile.changePage($("#list-page"));
+     $("#todo-list").append("<li>" + img_tag + "<h3>" + title + "</h3><p>" + body + "</p></li>")
+     $("#todo-list").listview('refresh');
+     $('#timer').yycountdown({endDateTime:'2018/11/24 00:00:00'});
 };
 
 
