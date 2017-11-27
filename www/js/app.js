837fa8a3f7427321738c3ef5322bdbd10b5588f0
iso-1 <kd1242983@st.kobedenshi.ac.jp>
2017-11-27 11:16:27 +0900

    通知とダイアログをループ処理にした。ダイアログがループするたびに重なって表示される

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,22 @@
-function addTodo(camera_url) {
-     var title = $("#todo-title").val();
-     var body = $("#todo-body").val();
-     var img_tag = "";
-     if (camera_url) {
-         img_tag = "<img src='" + camera_url + "'>";
-     }
-     $.mobile.changePage($("#list-page"));
-     $("#todo-list").append("<li>" + img_tag + "<h3>" + title + "</h3><p>" + body + "</p></li>")
-     $("#todo-list").listview('refresh');
-};
-
-
-
-
-
-
-var strDate = $("date-deadline").text();  // HTMLフォームの文字列
-var date = new Date(strDate);  // 日付
-var jsonDate = JSON.stringify(date); // JSON形式
-
+    function addTodoPicture() {
+      navigator.camera.getPicture(addTodo,
+      function() {
+        alert("Failed to get camera.");
+      }, {
+        quality : 50,
+        destinationType : Camera.DestinationType.FILE_URI,
+        targetWidth : 100,
+        targetHeight : 100
+      });
+    };
+    function addTodo(camera_url) {
+      var title = $("#todo-title").val();
+      var body = $("#todo-body").val();
+      var img_tag = "";
+      if (camera_url) {
+        img_tag = "<img src='" + camera_url + "'>";
+      }
+      $.mobile.changePage($("#list-page"));
+      $("#todo-list").append("<li>" + img_tag + "<h3>" + title + "</h3><p>" + body + "</p></li>")
+      $("#todo-list").listview('refresh');
+    };
